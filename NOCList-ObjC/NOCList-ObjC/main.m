0f6d49067d38f71ee1a4a2bf60654ebcc09000c8
Nicolas Rios <nickrios33@yahoo.com>
2020-05-20 18:26:15 -0700

    added Xcode project file to clones repo folder

---
+++ NOCList-ObjC/NOCList-ObjC/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  NOCList-ObjC
+//
+//  Created by Nicolas Rios on 5/20/20.
+//  Copyright © 2020 Nicolas Rios. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
