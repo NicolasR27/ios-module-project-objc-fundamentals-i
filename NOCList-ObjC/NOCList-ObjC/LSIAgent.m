625a5269fc92423c833014b90d334d306de5aa81
Nicolas Rios <nickrios33@yahoo.com>
2020-05-22 01:20:54 -0700

    added instance type and super initializer

--- NOCList-ObjC/NOCList-ObjC/LSIAgent.m
+++ NOCList-ObjC/NOCList-ObjC/LSIAgent.m
@@ -10,4 +10,17 @@
 
 @implementation LSIAgent
 
+- (instancetype)initWithCoverName:(NSString *)coverName realName: (NSString *)realName accessLevel: (int)accessLevel compromised: (BOOL)compromised {
+    
+    self = [super init];
+    if (self) {
+        _coverName = coverName;
+        _realName = realName;
+        _accessLevel = [NSNumber numberWithInt:accessLevel];
+        _compromised = [NSNumber numberWithBool:compromised];
+    }
+    return self;
+}
+
 @end
+
