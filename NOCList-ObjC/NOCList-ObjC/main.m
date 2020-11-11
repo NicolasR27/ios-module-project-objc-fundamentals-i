4b1bfdc6b7e6f39d5da6da6dd28895fd7b24dfe4
Nicolas Rios <nickrios33@yahoo.com>
2020-11-10 23:59:06 -0800

    Update main.m

--- NOCList-ObjC/NOCList-ObjC/main.m
+++ NOCList-ObjC/NOCList-ObjC/main.m
@@ -1,10 +1,3 @@
-//
-//  main.m
-//  NOCList-ObjC
-//
-//  Created by Nicolas Rios on 5/21/20.
-//  Copyright © 2020 Nicolas Rios. All rights reserved.
-//
 
 #import <Foundation/Foundation.h>
 #import "LSIAgent.h"
